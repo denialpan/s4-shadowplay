3ab6e12d09d230fc939030e21601116acd8755f2
Daniel <danpan1232@gmail.com>
2024-12-24 01:26:13 -0500

    Adjust timing one last time

--- src/components/ui/sidebar.jsx
+++ src/components/ui/sidebar.jsx
@@ -185,7 +185,7 @@ const Sidebar = React.forwardRef((
             {/* This is what handles the sidebar gap on desktop */}
             <div
                 className={cn(
-                    "duration-200 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] harsh-curve",
+                    "duration-300 relative h-svh w-[--sidebar-width] bg-transparent transition-[width] harsh-curve",
                     "group-data-[collapsible=offcanvas]:w-0",
                     "group-data-[side=right]:rotate-180",
                     variant === "floating" || variant === "inset"
@@ -194,7 +194,7 @@ const Sidebar = React.forwardRef((
                 )} />
             <div
                 className={cn(
-                    "duration-200 fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] harsh-curve md:flex",
+                    "duration-300 fixed inset-y-0 z-10 hidden h-svh w-[--sidebar-width] transition-[left,right,width] harsh-curve md:flex",
                     side === "left"
                         ? "left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]"
                         : "right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]",
@@ -357,7 +357,7 @@ const SidebarGroupLabel = React.forwardRef(({ className, asChild = false, ...pro
             ref={ref}
             data-sidebar="group-label"
             className={cn(
-                "duration-200 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opa] harsh-curve focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
+                "duration-300 flex h-8 shrink-0 items-center rounded-md px-2 text-xs font-medium text-sidebar-foreground/70 outline-none ring-sidebar-ring transition-[margin,opa] harsh-curve focus-visible:ring-2 [&>svg]:size-4 [&>svg]:shrink-0",
                 "group-data-[collapsible=icon]:-mt-8 group-data-[collapsible=icon]:opacity-0",
                 className
             )}
