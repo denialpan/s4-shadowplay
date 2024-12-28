542569bffc7f7bb60828925534d8341620752152
Daniel <danpan1232@gmail.com>
2024-12-28 02:42:51 -0500

    Add default admin admin on init database

--- database/init.js
+++ database/init.js
@@ -1,67 +1,86 @@
+const argon2 = require('argon2');
 const sqlite3 = require('sqlite3').verbose();
-
-// Path to the SQLite database file
 const db = new sqlite3.Database('./filesystem.db');
 
-db.serialize(() => {
+const username = "admin";
+const password = "admin";
+var hashedPassword = "";
 
-    // users table
-    db.run(`
-        CREATE TABLE IF NOT EXISTS users (
-            id TEXT PRIMARY KEY,
-            username TEXT UNIQUE NOT NULL,
-            password TEXT NOT NULL,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            admin BOOLEAN DEFAULT 1
-        )
-    `);
+const initializeDatabase = async () => {
+    try {
+        hashedPassword = await argon2.hash(password);
 
-    // folder table
-    db.run(`
-        CREATE TABLE IF NOT EXISTS folders (
-            id TEXT PRIMARY KEY,
-            name TEXT NOT NULL,
-            parent_id TEXT,
-            owner TEXT,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modified_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            FOREIGN KEY (parent_id) REFERENCES folders (id) ON DELETE CASCADE
-        );
-    `);
+        db.serialize(() => {
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS files (
-            id TEXT PRIMARY KEY,
-            s3_key TEXT NOT NULL,
-            name TEXT NOT NULL,
-            folder_id TEXT,
-            size INTEGER,
-            type TEXT,
-            owner TEXT,
-            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-            modified_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
-            FOREIGN KEY (folder_id) REFERENCES folders (id) ON DELETE CASCADE
-        );   
-    `)
+            // users table
+            db.run(`
+                CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    username TEXT UNIQUE NOT NULL,
+                    password TEXT NOT NULL,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    admin BOOLEAN DEFAULT 0
+                )
+            `);
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS tags (
-            id INTEGER PRIMARY KEY,
-            name TEXT UNIQUE NOT NULL
-        );
-    `)
+            // folder table
+            db.run(`
+                CREATE TABLE IF NOT EXISTS folders (
+                    id TEXT PRIMARY KEY,
+                    name TEXT NOT NULL,
+                    parent_id TEXT,
+                    owner TEXT,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    modified_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (parent_id) REFERENCES folders (id) ON DELETE CASCADE
+                );
+            `);
 
-    db.run(`
-        CREATE TABLE IF NOT EXISTS file_tags (
-            file_id TEXT NOT NULL,
-            tag_id INTEGER NOT NULL,
-            PRIMARY KEY (file_id, tag_id),
-            FOREIGN KEY (file_id) REFERENCES files (id) ON DELETE CASCADE,
-            FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
-        );
-    `)
+            // files table
+            db.run(`
+                CREATE TABLE IF NOT EXISTS files (
+                    id TEXT PRIMARY KEY,
+                    s3_key TEXT NOT NULL,
+                    name TEXT NOT NULL,
+                    folder_id TEXT,
+                    size INTEGER,
+                    type TEXT,
+                    owner TEXT,
+                    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                    modified_at DATETIME DEFAULT CURRENT_TIMESTAMP, 
+                    FOREIGN KEY (folder_id) REFERENCES folders (id) ON DELETE CASCADE
+                );   
+            `)
 
-    console.log("Filesystem database has been created.");
-});
+            // tags table
+            db.run(`
+                CREATE TABLE IF NOT EXISTS tags (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT UNIQUE NOT NULL
+                );
+            `)
 
-db.close();
+            // join files with tags
+            db.run(`
+                CREATE TABLE IF NOT EXISTS file_tags (
+                    file_id TEXT NOT NULL,
+                    tag_id INTEGER NOT NULL,
+                    PRIMARY KEY (file_id, tag_id),
+                    FOREIGN KEY (file_id) REFERENCES files (id) ON DELETE CASCADE,
+                    FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+                );
+            `)
+
+            db.run(
+                `INSERT INTO users (username, password, admin) VALUES (?, ?, ?)`, [username, hashedPassword, 1],
+            );
+
+            console.log("Filesystem database has been created.");
+        });
+
+    } finally {
+        db.close();
+    }
+}
+
+initializeDatabase();
